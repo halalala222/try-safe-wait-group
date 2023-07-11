0a36e8743007edb06a6d45b3991c50e3a44761f3
halalala222 <1741196223@qq.com>
2023-07-11 14:47:51 +0800

    feat:add multiErrors_test

---
+++ safe/errors/multiErrors_test.go
@@ -0,0 +1,44 @@
+package errors
+
+import (
+	"errors"
+	"testing"
+)
+
+var multiErrors MultiErrors
+
+var testErrors = []error{
+	errors.New("test1"),
+	errors.New("test2"),
+	errors.New("test3"),
+	errors.New("test4"),
+}
+
+func TestMultiErrors_Error(t *testing.T) {
+	multiErrors = append(multiErrors, testErrors...)
+	t.Log(multiErrors.Error())
+}
+
+func TestMultiErrors_ErrorOrNil(t *testing.T) {
+	if err := multiErrors.ErrorOrNil(); err != nil {
+		t.Errorf("expect nil , but get : [%v]", err)
+	}
+	multiErrors = append(multiErrors, testErrors...)
+	if err := multiErrors.ErrorOrNil(); err == nil {
+		t.Error("expect not nil , but get nil")
+	}
+}
+
+func TestMultiErrors_IsIn(t *testing.T) {
+	var notInError = errors.New("not in error")
+	if multiErrors.IsIn(notInError) {
+		t.Errorf("expect [%v] is not in, but get in", notInError)
+	}
+	multiErrors = append(multiErrors, testErrors...)
+	if multiErrors.IsIn(notInError) {
+		t.Errorf("expect [%v] is not in, but get in", notInError)
+	}
+	if !multiErrors.IsIn(testErrors[1]) {
+		t.Errorf("expect [%v] is in,but get not in", testErrors[1])
+	}
+}
